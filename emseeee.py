8fd44aea2d4f59e986a7a0dadb0b394b7380e223
Dmitry <astronom.v.cube@gmail.com>
2024-02-29 01:13:47 +0300

    теперь нормальное распределение, а не круг

--- emseeee.py
+++ emseeee.py
@@ -1,56 +1,103 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import emcee
+# import corner
+
+# # Определяем функцию, которую хотим минимизировать
+# def log_likelihood(theta):
+#     x, y = theta
+#     return 0.5 * (x**2 + y**2) + 2  # Пример функции: просто сумма квадратов
+
+# # Определяем априорное распределение (в данном случае - равномерное)
+# def log_prior(theta):
+#     x, y = theta
+#     if -10.0 < x < 10.0 and -10.0 < y < 10.0:
+#         return 0.0
+#     return -np.inf
+
+# # Полное логарифмическое правдоподобие
+# def log_probability(theta):
+#     lp = log_prior(theta)
+#     if not np.isfinite(lp):
+#         return -np.inf
+#     return lp - log_likelihood(theta)
+
+# # Настройка MCMC
+# ndim = 2  # Количество измерений (в данном случае, два параметра x и y)
+# nwalkers = 128  # Количество ходячих
+# nsteps = 10000  # Количество шагов в MCMC
+
+# # Инициализация начальных точек
+# initial = np.random.randn(nwalkers, ndim)
+
+# # Инициализация ходячих
+# sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, threads=12)
+
+# # Запуск MCMC
+# sampler.run_mcmc(initial, nsteps, progress=True)
+
+# # Получение цепей Марковских
+# samples = sampler.get_chain(discard=100, thin=15, flat=True)
+
+# # Найдем минимум в цепи Марковских
+# best_fit_index = np.argmax(sampler.flatlnprobability)
+# best_fit_params = sampler.flatchain[best_fit_index]
+
+# # Построение графика с использованием библиотеки corner
+# fig = corner.corner(samples, labels=['x', 'y'], truths=best_fit_params, plot_contours=True, fill_contours=True, levels=[0.2, 0.5, 0.8, 0.95], color='k', sharex = True)
+
+# plt.show()
+
+# plt.scatter(samples[:, 0], samples[:, 1])
+# plt.show()
+
+
+import matplotlib.pyplot as plt
 import numpy as np
 import emcee
 import corner
-import matplotlib.pyplot as plt
 
-# Определение вашей функции
-def my_function(x, y):
-    return x**2 + y**2
+# Определяем функцию, которую хотим минимизировать
+def log_likelihood(theta):
+    x, y, z = theta
+    return (x**2 + y**2 + z**2)  # Пример функции: просто сумма квадратов
 
-# Определение логарифма апостериорного распределения
-def log_likelihood(params):
-    x, y = params
-    return -0.5 * np.sum((my_function(x, y) - data) ** 2)
-
-def log_prior(params):
-    x, y = params
-    # Пример равномерного prior в пределах [-10, 10] для обоих параметров
-    if -15.0 < x < 15.0 and -15.0 < y < 15.0:
+# Определяем априорное распределение (в данном случае - равномерное)
+def log_prior(theta):
+    x, y, z = theta
+    if -10.0 < x < 10.0 and -10.0 < y < 10.0 and -10.0 < z < 10.0:
         return 0.0
     return -np.inf
 
-def log_probability(params):
-    lp = log_prior(params)
+# Полное логарифмическое правдоподобие
+def log_probability(theta):
+    lp = log_prior(theta)
     if not np.isfinite(lp):
         return -np.inf
-    return lp + log_likelihood(params)
+    return lp - log_likelihood(theta)
 
-# Генерация некоторых фиктивных данных
-np.random.seed(42)
-data = my_function(np.random.uniform(-15, 15, 1000), np.random.uniform(-15, 15, 1000)) + np.random.normal(0, 0.15, 1000)
+# Настройка MCMC
+ndim = 3  # Количество измерений (в данном случае, два параметра x и y)
+nwalkers = 512  # Количество ходячих
+nsteps = 10000  # Количество шагов в MCMC
 
-# Настройка emcee
-nwalkers = 10000
-ndim = 2
-nsteps = 500
+# Инициализация начальных точек
+initial = np.random.randn(nwalkers, ndim)
 
-# Инициализация позиций путем выбора случайных точек в пространстве параметров
-initial = np.random.uniform(-15, 15, (nwalkers, ndim))
+# Инициализация ходячих
+sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, threads=12)
 
 # Запуск MCMC
-sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, threads=12)
 sampler.run_mcmc(initial, nsteps, progress=True)
 
-# Получение цепей Маркова
-samples = sampler.get_chain(discard=100, thin=50, flat=True)
+# Получение Марковских цепей
+samples = sampler.get_chain(discard=100, thin=15, flat=True)
 
-print(samples)
+# Найдем минимум в цепи Марковских
+best_fit_index = np.argmax(sampler.flatlnprobability)
+best_fit_params = sampler.flatchain[best_fit_index]
+print(best_fit_params)
 
-# # Определение минимума
-# min_params = samples[np.argmax(log_probability(samples))]
-
-# Визуализация с помощью библиотеки corner
-fig = corner.corner(samples, labels=['x', 'y'], truths=[0, 0], range=[(-15, 15), (-15, 15)], plot_contours=True, fill_contours=True, levels=[-0.2, -0.5, -0.8, -0.95], color='k', reverse=True)
+# Построение графика с использованием библиотеки corner
+fig = corner.corner(samples, labels=['x', 'y', 'z'], truths=best_fit_params, plot_contours=True, fill_contours=True, levels=[0.5, 0.75, 0.9, 0.999], color='k', sharex = True)
 plt.show()
-
-
